e54d715184b38c7fcd54305d93864bba20d19304
mjarrettjohnson <miles@canva.com>
2021-07-19 15:56:15 +1000

    add videos

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('ready');
+  setTimeout(() => {
+    const body = document.getElementById('loading');
+    body.style.overflow = 'auto';
+
+    const [div] = document.getElementsByClassName('loading');
+    console.log('div', div);
+    div.style.display = 'none';
+
+  }, 5000);
 })
+
